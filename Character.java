605ca18ff916183c04c06afa814b63029b6ce179
waka <wa_morita@route-zero.com>
2019-03-09 18:55:27 +0900

    第2弾RPG
    damage,attackメソッドの追加
    CharacterNo,CharacterNumberの混在修正

--- Character.java
+++ Character.java
@@ -2,44 +2,79 @@
 public class Character {
 
 	static int _characterNumber=3;
-	int characterNo;
+	int characterID;
 	int hp;
 	int mp;
+	int ap;
 	String name;
 	String type;
 	
-	Character(int characterNo, int hp,int mp,String name,String type)
+	Character(int characterID, int hp,int mp,int ap,String name,String type)
 	{
-		this.characterNo=characterNo;
+		this.characterID=characterID;
 		this.hp=hp;
 		this.mp=mp;
+		this.ap=ap;
 		this.name=name;
 		this.type=type;
 	}
 	
 	public void selfProduce()
 	{
-		System.out.println("名前 : "+this.name);
-		System.out.println("体力 : "+this.hp);
-		System.out.println("魔力 : "+this.mp);
-		System.out.println("属性 : "+this.type);
-		if(this.characterNo==1) lucy();
-		if(this.characterNo==2) rightning();
-		if(this.characterNo==3) havoc();
+		System.out.println("名前 : "+name);
+		System.out.println("体力 : "+hp);
+		System.out.println("魔力 : "+mp);
+		System.out.println("属性 : "+type);
+		if(this.characterID==1) lucy();
+		if(this.characterID==2) rightning();
+		if(this.characterID==3) havoc();
+	}
+	public boolean judgeDeath()
+	{
+		if(hp<=0)
+		{
+			System.out.println(name+"は、たおれた");
+			return true;
+		}
+		else 
+		{
+			System.out.println(name+"の残りHPは"+hp+"!!");
+			return false;
+		}
+	}
+	
+	public void damage(int ap)
+	{
+		hp-=ap;
+		System.out.println(name+"は"+ap+"ポイントのダメージを受けた！！");
+	}
+	
+	public boolean magic(Character enemy)
+	{
+		System.out.println(name+"のまほう！！");
+		enemy.damage(mp);
+		return enemy.judgeDeath();
+	}
+	
+	public boolean attack(Character enemy)
+	{
+		System.out.println(name+"のこうげき！！");
+		enemy.damage(ap);
+		return enemy.judgeDeath();
 	}
 	
 	void lucy()
 	{
-		System.out.println("私は"+this.name+"魔法を使うのが得意！！\nサポートや回復は任せて！");
+		System.out.println("私は"+name+"魔法を使うのが得意！！\nサポートや回復は任せて！");
 	}
 	
 	void rightning()
 	{
-		System.out.println("私は"+this.name+"体力には自信がある！！\n前線で活躍するわ！！");
+		System.out.println("私は"+name+"体力には自信がある！！\n前線で活躍するわ！！");
 	}
 	void havoc()
 	{
-		System.out.println("私は"+this.name+"自慢はバランスの良さ！！\n遠隔攻撃が得意技！");
+		System.out.println("私は"+name+"自慢はバランスの良さ！！\n遠隔攻撃が得意技！");
 	}
 	
 }
--- RPG.java
+++ RPG.java
@@ -4,9 +4,10 @@ public class RPG {
 
 	public static void main(String[] args) {
 		System.out.println("ゲームを始めます。主人公を選択してください。");
-		Character lucy=new Character(1,1000,700,"Lucy","魔女");
-		Character rightning =new Character(2,1500,500,"Rightning","剣士");
-		Character havoc=new Character(3,1300,600,"Havoc","スナイパー");
+		Character lucy=new Character(1,1000,700,300,"Lucy","魔女");
+		Character rightning =new Character(2,1500,500,600,"Rightning","剣士");
+		Character havoc=new Character(3,1300,600,450,"Havoc","スナイパー");
+		Character enemy=new Character(4,500,300,100,"まっくろくろすけ","enemy");
 			
 		while(true)
 		{
@@ -22,13 +23,51 @@ public class RPG {
 			System.out.println("\n1:Lucy 2:Rightning 3:havoc");
 			System.out.println("キャラクターNoを選択してください");
 			int select=sc.nextInt();
-			if(select==lucy.characterNo)System.out.println(lucy.name+"を選択しました。");
-			if(select==rightning.characterNo)System.out.println(rightning.name+"を選択しました。");
-			if(select==havoc.characterNo)System.out.println(havoc.name+"を選択しました。");
+			Character mainCharacter=null;
+			if(select==lucy.characterID) 
+			{
+				mainCharacter=lucy;
+				select(mainCharacter);
+			}
+			if(select==rightning.characterID)
+			{
+				mainCharacter=rightning;
+				select(mainCharacter);
+			}
+			if(select==havoc.characterID)
+			{
+				mainCharacter=havoc;
+				select(mainCharacter);
+			}
 			if(select<1||select>Character._characterNumber)continue;
 			System.out.println("それでは冒険を始めましょう！！");
+			
+			System.out.println("\n敵が現れました！！\n攻撃してみましょう！！");
+			while(true)
+			{
+				System.out.println("\n1:こうげき 2:まほう");
+				System.out.println("選択してください");
+				select=sc.nextInt();
+				boolean judgeDeath=false;
+				if(select==1)
+				{
+					judgeDeath=mainCharacter.attack(enemy);
+				}
+				if(select==2)
+				{
+					judgeDeath=mainCharacter.magic(enemy);
+				}
+				if(select<1||select>2) continue;
+			
+				if(judgeDeath==true)break;
+			
+			}
 			System.exit(0);
 		
 		}
+	}
+	public static void select(Character character)
+	{
+		System.out.println(character.name+"を選択しました。");
 	}
 }
